c202808672fcbfed25a71bddbee18196cbf7cf55
Dixit Bhuva <mail@dixit.dev>
2025-03-16 04:38:01 +0000

    0.0.0.0 added

--- server.js
+++ server.js
@@ -64,4 +64,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 4000;
-server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+server.listen(PORT, '0.0.0.0', () => console.log(`Listening on port ${PORT}`));
